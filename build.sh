805c172fb4f53da45bcbd2f227ec2701d418d734
Pranesh K <kpranesh2004@gmail.com>
2025-03-19 07:54:59 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,23 @@
+# #!/bin/bash
+
+# # Variables
+# IMAGE_NAME="pranesh22/test"
+# TAG="latest"
+
+# # Build Docker image
+# docker build -t $IMAGE_NAME:$TAG .
+# echo "Docker image $IMAGE_NAME:$TAG built successfully."
+
 #!/bin/bash
+
+# Ensure the script exits if a command fails
+set -e
 
 # Variables
 IMAGE_NAME="pranesh22/test"
 TAG="latest"
 
 # Build Docker image
-docker build -t $IMAGE_NAME:$TAG .
+docker build -t "$IMAGE_NAME:$TAG" .
 echo "Docker image $IMAGE_NAME:$TAG built successfully."
+
